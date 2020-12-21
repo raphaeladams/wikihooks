8d017a04927d6ab3e4b0f4c8d2563f0e99d34ee0
Raphael Adams <raphael.adams@shopify.com>
2020-12-21 14:15:09 -0500

    Added fetch call during useEffect in SearchResults component

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactHtmlParser from "react-html-parser";
+
+import './SearchResults.css';
 
 export default function SearchResults() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    var url = "https://en.wikipedia.org/w/api.php"; 
+
+    var params = {
+        action: "query",
+        list: "search",
+        srsearch: "Helicopter",
+        format: "json"
+    };
+
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+    fetch(url)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(response) {
+          if (response.query.search[0].title === "Helicopter"){
+            // console.log("Your search page 'Helicopter' exists on English Wikipedia" );
+            setResults(response.query.search);
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+  }, []);
+
   return (
-    <div>Results go here</div>
+    <div>
+      {results.map(result => (
+        <div className="result">
+          <h2>{result.title}</h2>
+          <p>{ReactHtmlParser(result.snippet)}...</p>
+        </div>
+      ))}
+    </div>
   );
 }
