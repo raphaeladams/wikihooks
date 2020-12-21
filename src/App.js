f72cf5ccdf982d417537bc4861ee6392ac0da949
Raphael Adams <raphael.adams@shopify.com>
2020-12-21 10:33:48 -0500

    Rough scaffolding begun

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import InputField from './components/InputField';
+import SearchButton from './components/SearchButton';
+import SearchResults from './components/SearchResults';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br></br>
+      <InputField />
+      <SearchButton />
+      <br></br>
+      <br></br>
+      <SearchResults />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function SearchResults() {
+  return (
+    <div>Results go here</div>
+  );
+}
